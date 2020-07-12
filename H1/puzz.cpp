c529e9eef05bd35d35a746dc0d385bf33f1d8b14
baumanl <laurabauman12@gmail.com>
2020-07-11 23:48:16 -0700

    too slow. again. sad

--- H1/puzz.cpp
+++ H1/puzz.cpp
@@ -3,10 +3,14 @@
 #include <vector>
 using namespace std;
 
+//this code answers all the test cases I can think of, but it is too slow to pass the online test. 
+
+//used instead of a plain array because arrays can't be copied easily
 struct A {
 	int array[9];
 };
 
+//implement equality operater for struct A
 bool operator==(const A& lhs, const A& rhs){
 	bool res = true;
 	for (int i = 0; i < 9; i++){
@@ -22,62 +26,82 @@ void print_3by3(int in[]){
 	cout<<in[0]<<" "<<in[1]<<" "<<in[2]<<"\n";
 	cout<<in[3]<<" "<<in[4]<<" "<<in[5]<<"\n";
 	cout<<in[6]<<" "<<in[7]<<" "<<in[8]<<"\n";
+	cout<<"\n";
 }
 
 int solve(A in){
+
+	//arrays of all numbers that can be added to 1, 2, 3,...to form prime number sums
 	static int addends[9][4] = {{2, 4, 6}, {1, 3, 5, 9}, {2, 4, 8}, 
 	{1, 3, 7, 9}, {2, 6, 8}, {1, 5, 7}, {4, 6}, {3, 5, 9},
 	{2, 4, 8}};
+
+	//arrays of all indeces adjacent to indeces 0, 1, 2, ...
 	static int adjacents[9][4] = {{1, 3}, {0, 2, 4}, {1, 5}, 
 	{0, 4, 6}, {1, 3, 5, 7}, {2, 4, 8}, {3, 7}, {4, 6, 8}, {5, 7}};
+
+	//the end result grid
 	static A ideal;
 	for(int i = 0; i< 9; i++){
-		ideal.array[i] = i;
+		ideal.array[i] = i+1;
 	}
 
-	list <A> mods;
-	list <int> step_counter;
+	list <A> mods; //all variations of the grid that have to be altered
+	list <int> step_counter; //corresponding step counts to mods
 	
-	int min_steps = -1;
+	int min_steps = -1; //default number of steps and # returned if the grid is impossible
 	int steps, num;
-	A grid;
-	A temp;
+	A grid, temp;
 	vector<A> tried;
-	bool same, rep, loop;
+	bool rep, loop;
 	vector<int> swapped;
+	bool solved = false;
 
 	mods.push_back(in);
 	tried.push_back(in);
 	step_counter.push_back(min_steps);
 
-	while(mods.size() > 0){
+	while(mods.size() > 0){ //goes through all 
+		//initializes steps, grid, and alters lists to reflect they are being processed
 		steps = step_counter.front();
 		step_counter.pop_front();
 		grid = mods.front();
 		mods.pop_front();
 		swapped.clear(); 
-		if (min_steps == -1 || steps < min_steps-1){
-			same = true;
-			for(int i=0; i<9; i++){
-				rep = false;
-				for (auto j : swapped){
-					if (i == j){
-						rep = true;
-					}
+		//cout<<"steps: "<<steps<<"\n";
+		if (steps > 25){
+			break;
+		}
+
+		for(int i=0; i<9; i++){//goes through each position of a grid to make all possible swaps and add them to mods list
+			rep = false; //keeps track of if a swap has already been performed on a grid (1, 2) and (2, 1) shouldn't be repeated
+			for (auto j : swapped){
+				if (i == j){
+					rep = true;
 				}
-				if (rep == false){
-					num = grid.array[i];
-					if (num != ideal[i]){
-						same = false;
-						for (int j=0; j < 4 && (j < 2 || adjacents[i][j] != 0); j++){
-							int n = adjacents[i][j];
-							for (int m = 0; m < 4 && (m < 2 || addends[num-1][m] > 0); m++){
-								int k = addends[num-1][m];
-								if(grid.array[n] == k){
-									swapped.push_back(n);
-									temp = grid;
-									temp.array[i] = k;
-									temp.array[n] = num;
+			}
+			if (rep == false){
+				num = grid.array[i];
+				if (num != ideal.array[i]){
+					for (int j=0; j < 4 && (j < 2 || adjacents[i][j] != 0); j++){
+						int n = adjacents[i][j];
+						for (int m = 0; m < 4 && (m < 2 || addends[num-1][m] > 0); m++){
+							int k = addends[num-1][m];
+							if(grid.array[n] == k){
+								swapped.push_back(n);
+								temp = grid;
+								temp.array[i] = k;
+								temp.array[n] = num;
+								//print_3by3(temp.array);
+								if (temp == ideal){
+									//cout<<"Temp: "<<temp.array[0]<<temp.array[1]<<temp.array[2]<<temp.array[3]<<temp.array[4]<<temp.array[5]<<temp.array[6]<<temp.array[7]<<temp.array[8]<<"\n";
+									//cout<<"Temp: "<<ideal.array[0]<<ideal.array[1]<<ideal.array[2]<<ideal.array[3]<<ideal.array[4]<<ideal.array[5]<<ideal.array[6]<<ideal.array[7]<<ideal.array[8]<<"\n";
+									if (steps == -1){
+										return 1;
+									}else{
+										return steps+1;
+									}
+								}else{
 									loop = false;
 									for(auto z : tried){
 										if (temp == z){
@@ -87,29 +111,18 @@ int solve(A in){
 									if (loop == false){
 										mods.push_back(temp);
 										tried.push_back(temp);
-										print_3by3(temp.array);
-										cout<<"swapped: "<<k<<" and "<<num<<"\n";
 										if (steps == -1){
-											cout<<"steps: 1\n";
-											cout<<"\n";
 											step_counter.push_back(1);
 										}else{
 											step_counter.push_back(steps + 1);
-											cout<<"steps: "<<steps+1<<"\n";
-											cout<<"\n";
 										}
 									}
+									
 									break;
 								}
 							}
 						}
 					}
-				}
-			}
-			if(same == true){
-				if (min_steps == -1 || steps < min_steps){
-					min_steps = steps;
-					cout<<"GOT ONE: "<<min_steps<<"\n";
 				}
 			}
 		}
@@ -120,17 +133,15 @@ int solve(A in){
 int main(){
 	int t;
 	cin >> t;
-	cout<<"T: "<<t<<"\n";
-	cout<<"\n";
 
 	A input;
 	while(t-- > 0){
 		for (int i = 0; i < 9; i++){
 			cin >> input.array[i];
 		}
-		print_3by3(input.array);
+		//print_3by3(input.array);
 		int steps = solve(input);
-		cout<<"steps: "<<steps<<"\n";
+		cout<<steps<<"\n";
 	}
 
 	return 0;
